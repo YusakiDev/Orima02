85060c02c3c3b3d80c303844f74045f5295ab3d0
_Yusaki <36989640+Speedspencer@users.noreply.github.com>
2021-11-17 20:48:52 +0700

    Entity Commit

--- Orima02/Program.cs
+++ Orima02/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Orima02
+﻿using System;
+
+namespace Orima02
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
             
-            
+            Console.WriteLine("Hello World");
             
         }
     }
