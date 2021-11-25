0471ef32dc9d3451270391c4646d9ca0d16195ee
LoneGus <lonegusbeats@gmail.com>
2021-11-25 15:37:25 +0700

    edit2

--- Orima02/Game/Program.cs
+++ Orima02/Game/Program.cs
@@ -80,7 +80,12 @@ namespace Orima02
             {
                 "Narrator : You met the princess of the Feri City Kingdom……but in the forest?\n",
             });
-            
+            Scene scene1_3C2 = new Scene(1.2, 10, new[]
+            {
+                "Narrator : You met the princess of the Feri City Kingdom……but in the forest?\n",
+            });
+
+
             
   
 
